493b8697553f96c71a0247f59b6a6987cd349da5
atcorduneanu <andrew@andrewdomain.com>
2023-07-30 18:53:14 +0300

    move to linode provider

--- main.tf
+++ main.tf
@@ -1,28 +1,10 @@
-provider "aws" {
-  region = var.region
+provider "linode" {
+  token = var.Linode_token
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "linode_instance" "nanode-fedora" {
+  label = var.nanode-fedora-label
+  image = var.nanode-fedora-image
+  region = var.nanode-fedora-region
+  type = var.nanode-fedora-type
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,23 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+variable "nanode-fedora-region" {
+  description = "the region we're putting that particular box"
+  default = "eu-central" // which is Frankfurt - DE
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "nanode-fedora-type" {
+  description = "the type of box we want"
+  default = "g6-nanode-1"
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "nanode-fedora-label" {
+  description = "linode label"
+  default     = "fedoraBox-nanode"
 }
 
+variable "nanode-fedora-image" {
+  description = "image we're using for that nanode"
+  default     = "linode/Fedora38"
+}
+
+variable "Linode_token" {
+  type = string
+}
